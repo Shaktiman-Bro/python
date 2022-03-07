c4d14f2c67602ca0ce2d58a2fa639b333f5b3c13
Durgesh Mishra <67376628+Shaktiman-Bro@users.noreply.github.com>
2022-03-07 13:30:23 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,108 @@
+# # # # # # # setthis = ('this','is','set','for','testing')
+# # # # # # # print(setthis)
+# # # # # # # print(type(setthis))
+
+
+# # # # # # k = 33
+# # # # # # y = k / 5 * 32 - 23432 / 32 
+# # # # # # print(y)
+
+
+# # # # # nam = input('Who are you?')
+# # # # # print('Wllcome', nam)
+
+# # # # x = 5
+# # # # print("Before 5")
+# # # # if x == 5:
+# # # #     print("This is 5")
+# # # #     print("Still 5")
+# # # #     print("Is it still 5")
+
+# # # # y = 6
+# # # # x = 5
+# # # # print("Before 6")
+# # # # if x>y:
+# # # #     print("X is Greater than 6")
+# # # # else:
+# # # #     print("X is less then 6")
+
+# # # x = 5
+# # # print(x)
+# # # if x>0:
+# # #     print("Bigger than 2")
+# # #     if x<100:
+# # #         print("Less than 100")
+
+# # # print("All done 100")
+
+
+# # x = 5
+# # if x>10:
+# #     print("Yes it is bigger than")
+# # else:
+# #     print("It's not")
+
+# temp = "5"
+# cel = 0
+# fahr = float(temp)
+# cel = (fahr - 32.0) * 5.0 / 9.0
+# print(cel)
+
+# # x = 20
+# # if x > 10:
+# #     print('Smaller')
+# # elif x < 15:
+# #     print('Medium')
+# # else:
+# #     print("All done")
+# # # print("All Done")
+
+# test = float(input("Enter a number: "))
+# if test >= 0:
+#     if test == 0:
+#         print(test, "is Zero")
+#     else:
+#         print(test, "is Positive")  
+# else:
+#     print(test, 'Negative')
+ 
+
+# '''In this program, we input a number
+# check if the number is positive or
+# negative or zero and display
+# an appropriate message
+# This time we use nested if statement'''
+
+# # num = float(input("Enter a number: "))
+# # if num >= 0:
+# #     if num == 0:
+# #         print("Zero")
+# #     else:
+# #         print("Positive number")
+# # else:
+# #     print("Negative number")
+
+# strng = "Hello"
+# try:
+#     intiger = int(strng)
+# except:
+#     print("no need")
+# print("it's Done")
+
+
+# setting = input('Enter a number:')
+# try:
+#    val1 = int(setting)
+# except:
+#      val1 = -1
+# if val1 >= 0:
+#     print("Good Job!")  
+#     if val1 < 0:
+#         print("Try to put a positive number")
+# else:
+#     print("Try to put a number")
+# print ("Thankyou")
+        
+
+
+# Function in python

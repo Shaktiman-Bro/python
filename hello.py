f42af7f5ba519650971c3058a8348350bd8ece8a
Durgesh Mishra <67376628+Shaktiman-Bro@users.noreply.github.com>
2022-03-09 16:26:08 +0530

    Add files via upload

--- hello.py
+++ hello.py
@@ -1,108 +1,41 @@
-# # # # # # # setthis = ('this','is','set','for','testing')
-# # # # # # # print(setthis)
-# # # # # # # print(type(setthis))
+# from ctypes.wintypes import LARGE_INTEGER
+# from traceback import print_tb
+
+# larger = -1
+# print('Before:', larger )
+# for num in [12, 22, 23, 445, 545]:
+#     if num > larger:
+#         larger = num
+#        # print(larger, num)
+
+# print('After:', larger)
 
 
-# # # # # # k = 33
-# # # # # # y = k / 5 * 32 - 23432 / 32 
-# # # # # # print(y)
-
-
-# # # # # nam = input('Who are you?')
-# # # # # print('Wllcome', nam)
-
-# # # # x = 5
-# # # # print("Before 5")
-# # # # if x == 5:
-# # # #     print("This is 5")
-# # # #     print("Still 5")
-# # # #     print("Is it still 5")
-
-# # # # y = 6
-# # # # x = 5
-# # # # print("Before 6")
-# # # # if x>y:
-# # # #     print("X is Greater than 6")
-# # # # else:
-# # # #     print("X is less then 6")
-
-# # # x = 5
-# # # print(x)
-# # # if x>0:
-# # #     print("Bigger than 2")
-# # #     if x<100:
-# # #         print("Less than 100")
-
-# # # print("All done 100")
-
-
-# # x = 5
-# # if x>10:
-# #     print("Yes it is bigger than")
-# # else:
-# #     print("It's not")
-
-# temp = "5"
-# cel = 0
-# fahr = float(temp)
-# cel = (fahr - 32.0) * 5.0 / 9.0
-# print(cel)
-
-# # x = 20
-# # if x > 10:
-# #     print('Smaller')
-# # elif x < 15:
-# #     print('Medium')
-# # else:
-# #     print("All done")
-# # # print("All Done")
-
-# test = float(input("Enter a number: "))
-# if test >= 0:
-#     if test == 0:
-#         print(test, "is Zero")
+# valu = False
+# num = 5 
+# for value in [12, 22, 212, 1323, 4421, 3, 33, 9, 4]:
+#     if value == num:
+#         valu = True
+#         print(value, valu)
 #     else:
-#         print(test, "is Positive")  
-# else:
-#     print(test, 'Negative')
- 
-
-# '''In this program, we input a number
-# check if the number is positive or
-# negative or zero and display
-# an appropriate message
-# This time we use nested if statement'''
-
-# # num = float(input("Enter a number: "))
-# # if num >= 0:
-# #     if num == 0:
-# #         print("Zero")
-# #     else:
-# #         print("Positive number")
-# # else:
-# #     print("Negative number")
-
-# strng = "Hello"
-# try:
-#     intiger = int(strng)
-# except:
-#     print("no need")
-# print("it's Done")
+#         valu = False
+#         print(valu)  
+# print("Done")
 
 
-# setting = input('Enter a number:')
-# try:
-#    val1 = int(setting)
-# except:
-#      val1 = -1
-# if val1 >= 0:
-#     print("Good Job!")  
-#     if val1 < 0:
-#         print("Try to put a positive number")
-# else:
-#     print("Try to put a number")
-# print ("Thankyou")
+# smalest_num = None
+# for val in [2, 12, 34, 335, 535, 3535, 1]:
+#     if smalest_num is None :
+#         smalest_num = val    
+#     elif val < smalest_num:
+#         smalest_num = val
+#         print(smalest_num , val)
         
+# print("After", smalest_num)
 
 
-# Function in python
+from itertools import count
+
+
+word = 'banana'
+print(word[0:3])
